4280a9d8c32390ac97ffefbe00dad569dcdde415
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-09-28 16:55:31 +0200

    Added footer section without the content

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+	<div className='container'>
+		<div className='footer'>
+			<h3 className='footer__brand'>Shortly</h3>
+		</div>
+	</div>
+)
+
+export default Footer
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import Heading from './Heading'
 import Statistics from './Statistics'
 import Boost from './Boost'
+import Footer from './Footer'
 
 // Could be refactored to something cleaner?
 const Home = () => (
@@ -12,6 +13,9 @@ const Home = () => (
 		</div>
 		<div className='section-boost'>
 			<Boost />
+		</div>
+		<div className='section-footer'>
+			<Footer />
 		</div>
 	</Fragment>
 )
