008119b5d43ab294f30650323f7abf5b8ab00823
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-09-28 14:22:51 +0200

    Added 2 solutions for positioning the img, one with pseudo elements the other with HTML img tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import img from '../images/illustration-working.svg'
 
 const Header = () => (
-	<div className='header'>
-		<h1 className='header__title'>More than just shorter links</h1>
-		<p className='header__sub'>
-			Build your brand's recognitions and get detailed insights on how your
-			links are performing.
-		</p>
-		<button className='header__cta btn'>Get Started</button>
-	</div>
+	<Fragment>
+		<div className='header'>
+			<h1 className='header__title'>More than just shorter links</h1>
+			<p className='header__sub'>
+				Build your brand's recognitions and get detailed insights on how your
+				links are performing.
+			</p>
+			<button className='header__cta btn'>Get Started</button>
+		</div>
+		<img src={img} alt='Person working behind a desk' className='header__img' />
+	</Fragment>
 )
 
 export default Header
