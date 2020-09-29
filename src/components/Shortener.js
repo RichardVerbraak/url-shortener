8d268eb38bbdb972733d15211ffc4eb39ee51262
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-09-29 16:50:23 +0200

    Fetching data from rel.link API working

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 
 // TODO: Add a section underneath the shortener for showing the newly generated URLS
-const Shortener = () => (
-	<div className='shortener__container'>
-		<div className='input__container'>
-			<input
-				type='text'
-				className='input__container--input'
-				placeholder='Shorten a link here...'
-			/>
-			<button className='input__container--btn btn'>Shorten It!</button>
-		</div>
-		<p className='input__container--sub'>Please add a link</p>
-	</div>
-)
+const Shortener = () => {
+	const [url, setUrl] = useState('')
+
+	const request = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ url }),
+	}
+
+	const shortenURL = async () => {
+		const res = await fetch('https://rel.ink/api/links/', request)
+
+		const data = await res.json()
+
+		console.log(data.hashid)
+	}
+
+	return (
+		<Fragment>
+			<div className='shortener__container'>
+				<div className='input__content'>
+					<div className='input__container'>
+						<input
+							type='text'
+							className='input__container--input'
+							placeholder='Shorten a link here...'
+							onChange={(e) => {
+								setUrl(e.target.value)
+							}}
+						/>
+						<button className='input__container--btn btn' onClick={shortenURL}>
+							Shorten It!
+						</button>
+					</div>
+
+					{!url && <p className='input__container--sub'>Please add a link</p>}
+				</div>
+			</div>
+		</Fragment>
+	)
+}
 
 export default Shortener
